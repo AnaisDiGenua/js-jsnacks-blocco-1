9429c952929d8381ca06e340a584d1e9bf514dc4
AnaisDiGenua <anais.digenua5@gmail.com>
2021-11-15 15:36:14 +0100

    jsnack 2

--- Jsnack 2/js/script.js
+++ Jsnack 2/js/script.js
@@ -1,10 +1,22 @@
-// JSnack 1
-// Il software deve chiedere per 5 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
-// Esegui questo programma in due versioni, con il for e con il while
 // JSnack 2
 // Dare la possibilità di inserire due parole.
 // Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
 // Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
-// JSnack 3
-// Stampa le potenze di 2 fino a 1000.
+
+
+
+
+// chiedere all'utente due parole
+let firstWord = prompt('scrivi una parola');
+let secondWord = prompt('scrivi un\'altra parola');
+
+// istruzioni condizionali
+if (firstWord.length == secondWord.length) {
+    console.log(firstWord + ' ' + secondWord);
+} else if (firstWord.length > secondWord.length) {
+    console.log(firstWord);
+} else {
+    console.log(secondWord);
+}
+
+
